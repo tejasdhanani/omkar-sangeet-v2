7e67228c82e042da7ab9624d963231f86447934a
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-08-17 16:40:51 -0700

    installed prettier plugin tailwind

---
+++ content/metadata.js
@@ -0,0 +1,5 @@
+export const home_metadata = {
+    title: "Omkar Sangeet Mahavidyalaya | Indian Classical Music Academy",
+    description: "Join Omkar Sangeet Mahavidyalaya for expert training in Vocal, Harmonium, Guitar, Piano, Keyboard, Kathak, and more. Discover the beauty of Indian classical music with our experienced instructors.",
+};
+
