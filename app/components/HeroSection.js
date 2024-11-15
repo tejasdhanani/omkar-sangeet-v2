22caa9cc2ef67854f2f908041bed353205ad7879
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-11-15 06:15:49 -0800

    Hero Section for homepage

---
+++ app/components/HeroSection.js
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import PropTypes from "prop-types";
+
+export default function HeroSection({
+  imageSrc,
+  imageAltText,
+  ButtonAction,
+  bgFilterOpacity = 50,
+  HeroTitle = "Default Title",
+  HeroSubTitle = "Default Subtitle",
+  ButtonText = "Click Me",
+}) {
+  return (
+    <section className="relative h-screen w-full">
+      {/* Background Image */}
+      <Image
+        src={imageSrc} // Replace with your image path
+        alt={imageAltText}
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="absolute inset-0 -z-10"
+        priority
+      />
+
+      <div
+        className={"absolute inset-0 -z-10 bg-black"}
+        style={{
+          opacity: bgFilterOpacity / 100,
+        }}
+      ></div>
+
+      {/* Centered Text */}
+      <div className="relative flex h-full w-full flex-col items-center justify-center px-4 text-center text-white md:px-8">
+        <h1 className="text-3xl font-bold md:text-5xl lg:text-6xl">
+          {HeroTitle}
+        </h1>
+        <p className="mt-4 text-lg md:mt-6 md:text-2xl lg:text-3xl">
+          {HeroSubTitle}
+        </p>
+        <button
+          onClick={ButtonAction}
+          aria-label={ButtonText}
+          className="mt-8 rounded-lg bg-primary px-6 py-2 text-base font-medium text-white transition-colors duration-300 hover:bg-secondary md:px-8 md:py-3 md:text-lg"
+        >
+          {ButtonText}
+        </button>
+      </div>
+    </section>
+  );
+}
+
+HeroSection.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  imageAltText: PropTypes.string.isRequired,
+  bgFilterOpacity: PropTypes.number,
+  HeroTitle: PropTypes.string,
+  HeroSubTitle: PropTypes.string,
+  ButtonText: PropTypes.string,
+  ButtonAction: PropTypes.func,
+  overlayEnabled: PropTypes.bool,
+  additionalClasses: PropTypes.string,
+};
--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
+"use client";
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <HeroSection
+      imageSrc="/lohri-celebration-india.jpg"
+      imageAltText="all types of instruments"
+      ButtonAction={() => alert("Button Clicked")}
+      bgFilterOpacity={50}
+      HeroTitle="Welcome to Omkar Sangeet Mahavidyalay"
+      HeroSubTitle="Experience the Essence of Indian Classical Music"
+      ButtonText="Learn More"
+    />
   );
 }
