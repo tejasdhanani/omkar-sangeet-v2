47171d9621fe34d5e42eb23bc7ee89d7b4c8009c
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-11-18 21:12:51 -0800

    content updated

--- app/components/AboutUs.js
+++ app/components/AboutUs.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function AboutUs() {
   return (
-    <section className="bg-gray-100 py-10">
+    <section className="py-10">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <h2 className="mb-6 text-center text-3xl font-extrabold text-gray-900">
           About Us
@@ -21,23 +21,13 @@ export default function AboutUs() {
           {/* Text Section */}
           <div className="lg:w-1/2">
             <p className="mt-4 text-lg text-gray-700">
-              We are affiliated with{" "}
-              <Link
-                className="text-secondary underline"
-                href="https://abgmvm.org"
-                target="_blank"
-              >
-                <strong>
-                  Akhil Bhartiya Gandharva Mahavidyalaya Mandal Mumbai
-                </strong>
-              </Link>{" "}
-              (Government organization) and we are located in Ahmedabad. It is
-              special place where you can discover the wonderful world of Indian
-              classical music. Led by Guruji,{" "}
-              <strong>Shree Acharya Dhananjay</strong>, along with a team of
-              experienced teachers, we offer an environment where students of
-              all levels can explore the magical melodies and rhythms of Indian
-              classical music.
+              We are located in Ahmedabad and it is a special place where you
+              can discover the wonderful world of Indian classical music. Led by
+              Guruji,{" "}
+              <strong className="text-primary">Shri. Dhananjay Acharya</strong>,
+              along with a team of experienced teachers, we offer an environment
+              where students of all levels can explore the magical melodies and
+              rhythms of Indian classical music.
             </p>
             <p className="mt-4 text-lg text-gray-700">
               Whether you are just starting out or already have some experience,
